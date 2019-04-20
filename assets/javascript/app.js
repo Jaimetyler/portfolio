38fe722c5967c0d113d26760c1b500638a54ae02
Jaime Tyler <jaimetyler88@gmail.com>
2019-04-19 23:57:53 -0400

    added top bar that comes in on button click with picture and resume pdf link. styled the buttons and added some hover effects.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,3 +3,16 @@ console.log("yo im working!")
 $("#submit").on("click", function(){
     console.log("button clicked");
 })
+
+$("#port-button").on("click", function(){
+    console.log("port-button working")
+    $(".sidebar").show();
+    $("#port-button").hide();
+})
+
+$(".exit-sidebar").on("click", function(){
+    console.log("exit-sidebar working")
+    $(".sidebar").hide();
+    $("#port-button").show();
+    $("#exit-sidebar").hide();
+})

98e972d5550f9e69de270c1288c614a0478a85fb
Jaime Tyler <jaimetyler88@gmail.com>
2019-06-20 17:54:50 -0400

    trying to get portfolio

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -17,6 +17,56 @@ $(".exit-sidebar").on("click", function(){
     $("#exit-sidebar").hide();
 })
 
+$("#trivia-link").on("click", function() {
+    preventDefault();
+    $("#trivia-content").show();
+
+    // $("#gifApp-content").hide();
+    // $("#friendFinder-content").hide();
+    // $("#crystalCollector-content").hide();
+    // $("#train-content").hide();
+})
+$("#friendFinder-link").on("click", function() {
+    $("#friendFinder-content").show();
+
+    // $("#trivia-content").hide();
+    // $("#gifApp-content").hide();
+    // $("#crystalCollector-content").hide();
+    // $("#train-content").hide();
+})
+$("#crystalCollector-link").on("click", function() {
+    $("#crystalCollector-content").show();
+
+    // $("#trivia-content").hide();
+    // $("#gifApp-content").hide();
+    // $("#friendFinder-content").hide();
+    // $("#train-content").hide();
+})
+$("#train-link").on("click", function() {
+    $("#train-content").show();
+
+    // $("#trivia-content").hide();
+    // $("#gifApp-content").hide();
+    // $("#friendFinder-content").hide();
+    // $("#crystalCollector-content").hide();
+})
+$("#gifApp-link").on("click", function() {
+    $("#gifApp-content").show();
+
+    // $("#trivia-content").hide();
+    // $("#friendFinder-content").hide();
+    // $("#crystalCollector-content").hide();
+    // $("#train-content").hide();
+})
+
+$(window).on("load", function() {
+    $("#trivia-content").hide();
+    $("#gifApp-content").hide();
+    $("#friendFinder-content").hide();
+    $("#crystalCollector-content").hide();
+    $("#train-content").hide();
+})
+
 function w3_open() {
     document.getElementById("mySidebar").style.display = "block";
   }
